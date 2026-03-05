73b3dc42c5c1fa7aded8c83b34075ed72b56fe9b
Tengda Fan <1012512411@qq.com>
2026-03-05 16:15:09 +0800

    main: 测试actons的功能

---
+++ test_demo.py
@@ -0,0 +1,11 @@
+"""Simple test file to satisfy pytest requirements."""
+
+
+def test_example():
+    """Example test that always passes."""
+    assert True
+
+
+def test_basic_assertion():
+    """Basic assertion test."""
+    assert 1 + 1 == 2
